8142df4f06e554f9716cda04d99c936876c91b02
Gatien BOVYN <gbovyn@wemanity.com>
2020-11-30 22:03:51 +0100

    [refactor] Extract common item update logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,21 +30,23 @@ class GildedRose {
             return;
         }
 
-        if (item.quality > 0) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-            }
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
+        updateCommonItem(item);
+    }
+
+    private void updateCommonItem(final Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
+
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
     }
