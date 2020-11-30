2c237a8c9cc24c94afb8aec0375f3548c0f1d9e7
Gatien BOVYN <gbovyn@wemanity.com>
2020-11-30 22:07:54 +0100

    [refactor] Extract "Backstage Passes" in its own class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -26,7 +26,7 @@ class GildedRose {
         }
 
         if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            updateBackstagePasses(item);
+            new BackstagePassService().update(item);
             return;
         }
 
@@ -48,28 +48,6 @@ class GildedRose {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
-        }
-    }
-
-    private void updateBackstagePasses(final Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-            item.quality = 0;
         }
     }
 }
