beda2d684656c5df8d1852830cf09374897af594
Gatien BOVYN <gbovyn@wemanity.com>
2020-11-30 22:06:28 +0100

    [refactor] Extract "Aged Brie" in its own class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,7 +21,7 @@ class GildedRose {
     private void update(final Item item) {
 
         if (item.name.equals("Aged Brie")) {
-            updateAgedBrie(item);
+            new AgedBrieService().update(item);
             return;
         }
 
@@ -70,20 +70,6 @@ class GildedRose {
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
             item.quality = 0;
-        }
-    }
-
-    private void updateAgedBrie(final Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
         }
     }
 }
