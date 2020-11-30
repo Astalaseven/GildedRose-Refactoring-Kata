6067d8fdf55f6db4c61de9577ae11da790c305c4
Gatien BOVYN <gbovyn@wemanity.com>
2020-11-30 22:11:04 +0100

    [refactor] Extract common item in its own class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -34,20 +34,6 @@ class GildedRose {
             return;
         }
 
-        updateCommonItem(item);
-    }
-
-    private void updateCommonItem(final Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-        }
+        new CommonItemService().update(item);
     }
 }
